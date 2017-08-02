243c3b3fcbcfc889b0f7608455daa8051f171154
ManojParmar-BTC <manoj.parmar@botreetechnologies.com>
2017-08-02 16:44:32 +0530

    check if invalid csv file uploaded

--- app/controllers/record_csvs_controller.rb
+++ app/controllers/record_csvs_controller.rb
@@ -7,15 +7,22 @@ class RecordCsvsController < ApplicationController
 
   def save
     counts = 0
-    CSV.foreach(params[:csv_file].path, headers: true) do |row|
-      if(row['name'].present? && row['email'].present?)
-        user = User.find_or_create_by(email: row['email'])
-        user_data = {name: row['name'], email: row['email'], age: row['age'], address: row['address']}
-        counts += 1 if user.update(user_data)
+    if params[:csv_file].present?
+      unless params[:csv_file].content_type == 'text/csv'
+        raise StandardError.new('please upload valid csv file.')
       end
+      CSV.foreach(params[:csv_file].path, headers: true) do |row|
+        if(row['name'].present? && row['email'].present?)
+          user = User.find_or_create_by(email: row['email'])
+          user_data = {name: row['name'], email: row['email'], age: row['age'], address: row['address']}
+          counts += 1 if user.update(user_data)
+        end
+      end
+      flash[:success] = "#{counts} records updated."
+      redirect_to root_path
+    else
+
     end
-    flash[:success] = "#{counts} records updated."
-    redirect_to root_path
   rescue => exception
     flash[:danger] = "Error: #{exception}"
     redirect_to root_path
